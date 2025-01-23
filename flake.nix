9a6c5a1b75b17dca57bb9101e57502c22d912324
Victor Danek <viktordanek10@gmail.com>
2025-01-23 11:26:02 -0500

    rethink

--- flake.nix
+++ flake.nix
@@ -197,7 +197,7 @@
                                                         target = "e55dd2c8db9b224d0d6207c430354f481ece26fbf458400726e7624bcc79fcb72de81bccc55a066ebfa569317862dec4b13ea6bb4b1e8b0300f1dc867e51503d" ;
                                                         temporary =
                                                             {
-                                                                temporary =
+                                                                # temporary =
                                                                     let
                                                                         mapper = builtins.map ( v : { name = v.name ; value = v.value ; } ) temporary ;
                                                                         in builtins.listToAttrs ( builtins.map mapper temporary ) ;
