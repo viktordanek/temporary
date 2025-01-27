a3b56f4cd54af77745b5d928d149f45ccdda7ffa
Victor Danek <viktordanek10@gmail.com>
2025-01-27 16:53:16 -0500

    WORKS

--- scripts/test/util/post/observed/init.sh
+++ scripts/test/util/post/observed/init.sh
@@ -1,10 +1,10 @@
 TARGET=${e55dd2c8db9b224d0d6207c430354f481ece26fbf458400726e7624bcc79fcb72de81bccc55a066ebfa569317862dec4b13ea6bb4b1e8b0300f1dc867e51503d} &&
-${FIND} ${@} -mindepth 1 -maxdepth 1 -type f | ${SORT} | while read FILE
-do
-  BASE=$( ${BASENAME} ${FILE} ) &&
-    ${ECHO} "<" >> ${TARGET} &&
-    ${ECHO} ${BASE} >> ${TARGET} &&
-    ${CAT} ${FILE} >> ${TARGET} &&
-    ${ECHO} ">" >> ${TARGET} &&
-    ${ECHO} >> ${TARGET}
-  done
+  ${FIND} ${@} -mindepth 1 -maxdepth 1 -type f | ${SORT} | while read FILE
+  do
+    BASE=$( ${BASENAME} ${FILE} ) &&
+      ${ECHO} "<" >> ${TARGET} &&
+      ${ECHO} ${BASE} >> ${TARGET} &&
+      ${CAT} ${FILE} >> ${TARGET} &&
+      ${ECHO} ">" >> ${TARGET} &&
+      ${ECHO} >> ${TARGET}
+    done
