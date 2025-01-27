0c5f269c8cb124343545426a89233d629a44835e
Victor Danek <viktordanek10@gmail.com>
2025-01-27 13:28:58 -0500

    better

--- flake.nix
+++ flake.nix
@@ -362,7 +362,7 @@
                                                                             exit 10
                                                                     '' ;
                                                     } ;
-                           /nix/store/fm07a7iqxsnmf9s2x5kpanas7f63jyiz-temporary-test         lib = lib ;
+                                    lib = lib ;
                                 } ;
                 in flake-utils.lib.eachDefaultSystem fun ;
 }
