ec04bcf6b615438fd9fefe12d0511bb20598f969
Victor Danek <viktordanek10@gmail.com>
2025-01-31 14:59:29 -0500

    YES

--- scripts/test/util/re-observate.sh
+++ scripts/test/util/re-observate.sh
@@ -1,5 +1,7 @@
-${ECHO} "echo : resources :" > scripts/test/util/observed-internal.nix &&
+${ECHO} "echo : resources : sleep : find : grep : wc :" > scripts/test/util/observed-internal.nix &&
   ${ECHO} "  {" >> scripts/test/util/observed-internal.nix &&
   ${SED} -e "s#^#    #" ${OBSERVATE_FILE} >> scripts/test/util/observed-internal.nix &&
   ${ECHO} >> scripts/test/util/observed-internal.nix &&
+  ${ECHO} "    ${SLEEP} 10s" >> scripts/test/util/observed-internal.nix &&
+  ${ECHO} "    ${FIND} /build -mindepth 2 -maxdepth 2 -type f -name temporary -exec ${GREP} ^temporary {} \; | ${WC} --lines > /build/observed/temporary/count.mid" > scripts/test/util/observed-internal.nix
   ${ECHO} "  }" >> scripts/test/util/observed-internal.nix
