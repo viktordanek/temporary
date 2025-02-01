94cba6bc0d3d3ff101aa72064fbddc647d51f7a9
Victor Danek <viktordanek10@gmail.com>
2025-01-31 20:13:30 -0500

    WORKS

--- scripts/test/temporary/init.sh
+++ scripts/test/temporary/init.sh
@@ -20,7 +20,7 @@ TARGET=${e55dd2c8db9b224d0d6207c430354f481ece26fbf458400726e7624bcc79fcb72de81bc
     HAS_STANDARD_INPUT=false
       STANDARD_INPUT=
   fi &&
-  ${ECHO} INIT ${STANDARD_OUTPUT} &&
+  ${ECHO} ${STANDARD_OUTPUT} &&
   ${ECHO} "init:" > ${TARGET} &&
   ${ECHO} "  name: ${NAME}" >> ${TARGET} &&
   ${ECHO} "  has_standard_input: ${HAS_STANDARD_INPUT}" >> ${TARGET} &&
--- scripts/test/temporary/release.sh
+++ scripts/test/temporary/release.sh
@@ -16,7 +16,7 @@ TARGET=${e55dd2c8db9b224d0d6207c430354f481ece26fbf458400726e7624bcc79fcb72de81bc
     HAS_STANDARD_INPUT=false
       STANDARD_INPUT=
   fi &&
-  ${ECHO} RELEASE ${STANDARD_OUTPUT} &&
+  ${ECHO} ${STANDARD_OUTPUT} &&
   ${ECHO} "release:" >> ${TARGET} &&
   ${ECHO} "  name: ${NAME}" >> ${TARGET} &&
   ${ECHO} "  has_standard_input: ${HAS_STANDARD_INPUT}" >> ${TARGET} &&
