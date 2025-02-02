8592fbd47f90c757925c13e9fcac50e340a57541
Victor Danek <viktordanek10@gmail.com>
2025-02-01 19:14:00 -0500

    status is wrong

--- scripts/test/util/post/expected/init.sh
+++ scripts/test/util/post/expected/init.sh
@@ -24,9 +24,9 @@ TARGET=${e55dd2c8db9b224d0d6207c430354f481ece26fbf458400726e7624bcc79fcb72de81bc
       --arg ARGUMENTS $( hash "arguments" 0 ) \
       --arg INIT_STANDARD_ERROR $( hash "init-standard-error" 0 ) \
       --arg INIT_STANDARD_OUTPUT $( hash "init-standard-output" 0 ) \
-      --argjson INIT_STATUS $( val 4 2 "init-status") \
+      --argjson INIT_STATUS $( val 2 2 "init-status") \
       --arg RELEASE_STANDARD_ERROR $( hash "release-standard-error" 0 ) \
       --arg RELEASE_STANDARD_OUTPUT $( hash "release-standard-output" 0 ) \
-      --argjson RELEASE_STATUS $( val 8 2 "release-status" ) \
+      --argjson RELEASE_STATUS $( val 4 2 "release-status" ) \
       "$( ${SED} ${TEMPLATE_FILE} -e "s#\"\$\(\w\+\)\"#\$\1#g" )" \
       > ${TARGET}
