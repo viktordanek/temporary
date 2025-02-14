fd9d4bc622c8303bf95fba104cb06ac7a28c8682
Victor Danek <viktordanek10@gmail.com>
2025-02-14 07:47:29 -0500

    renamed RRRR

--- scripts/implementation/setup.sh
+++ scripts/implementation/setup.sh
@@ -1,6 +1,6 @@
-export RRRR=$( ${MKTEMP} --directory -t ${TEMPORARY_RESOURCE_MASK} ) &&
-  ${ECHO} "${@}" > ${RRRR}/init.arguments &&
-  ${CHMOD} 0400 ${RRRR}/init.arguments &&
+export RESOURCE=$( ${MKTEMP} --directory -t ${TEMPORARY_RESOURCE_MASK} ) &&
+  ${ECHO} "${@}" > ${RESOURCE}/init.arguments &&
+  ${CHMOD} 0400 ${RESOURCE}/init.arguments &&
   PARENT_PID=$( ${PS} -p ${$} -o ppid= ) &&
   GRANDPARENT_PID=$( ${PS} -p ${PARENT_PID} -o ppid= ) &&
   GREAT_GRANDPARENT_PID=$( ${PS} -p ${GRANDPARENT_PID} -o ppid= ) &&
@@ -12,57 +12,57 @@ export RRRR=$( ${MKTEMP} --directory -t ${TEMPORARY_RESOURCE_MASK} ) &&
   then
     # TARGET_PID=$( ${PS} -p ${GREAT_GRANDPARENT_PID} -o ppid= ) &&
     TARGET_PID=$( ${PS} -p ${GRANDPARENT_PID} -o ppid= ) &&
-      ${TEE} > ${RRRR}/init.standard-input &&
-      ${CHMOD} 0400 ${RRRR}/init.standard-input
+      ${TEE} > ${RESOURCE}/init.standard-input &&
+      ${CHMOD} 0400 ${RESOURCE}/init.standard-input
   elif [ -f /proc/self/fd/0 ]
   then
     # TARGET_PID=$( ${PS} -p ${GRANDPARENT_PID} -o ppid= ) &&
     TARGET_PID=$( ${PS} -p ${GRANDPARENT_PID} -o ppid= ) &&
-      ${CAT} > ${RRRR}/init.standard-input &&
-      ${CHMOD} 0400 ${RRRR}/init.standard-input
+      ${CAT} > ${RESOURCE}/init.standard-input &&
+      ${CHMOD} 0400 ${RESOURCE}/init.standard-input
   else
     # TARGET_PID=$( ${PS} -p ${GRANDPARENT_PID} -o ppid= )
     TARGET_PID=$( ${PS} -p ${PARENT_PID} -o ppid= )
   fi &&
   if [ -x ${INIT} ]
   then
-    ${LN} --symbolic ${INIT} ${RRRR}/init.sh
+    ${LN} --symbolic ${INIT} ${RESOURCE}/init.sh
   fi &&
   if [ -x ${RELEASE} ]
   then
-    ${LN} --symbolic ${RELEASE} ${RRRR}/release.sh
+    ${LN} --symbolic ${RELEASE} ${RESOURCE}/release.sh
   fi &&
   if [ -x ${POST} ]
    then
-     ${LN} --symbolic ${POST} ${RRRR}/post.sh
+     ${LN} --symbolic ${POST} ${RESOURCE}/post.sh
   fi &&
-  ${LN} --symbolic ${TEARDOWN_SYNCH} ${RRRR}/teardown-synch.sh &&
-  ${LN} --symbolic ${TEARDOWN_ASYNCH} ${RRRR}/teardown-asynch.sh &&
-  if [ -x ${RRRR}/init.sh ]
+  ${LN} --symbolic ${TEARDOWN_SYNCH} ${RESOURCE}/teardown-synch.sh &&
+  ${LN} --symbolic ${TEARDOWN_ASYNCH} ${RESOURCE}/teardown-asynch.sh &&
+  if [ -x ${RESOURCE}/init.sh ]
   then
-    if [ -f ${RRRR}/init.standard-input ] && ${CAT} ${RRRR}/init.standard-input | ${RRRR}/init.sh $( ${CAT} ${RRRR}/init.arguments ) > ${RRRR}/init.standard-output 2> ${RRRR}/init.standard-error
+    if [ -f ${RESOURCE}/init.standard-input ] && ${CAT} ${RESOURCE}/init.standard-input | ${RESOURCE}/init.sh $( ${CAT} ${RESOURCE}/init.arguments ) > ${RESOURCE}/init.standard-output 2> ${RESOURCE}/init.standard-error
     then
       STATUS=${?}
-    elif ${RRRR}/init.sh $( ${CAT} ${RRRR}/init.arguments ) > ${RRRR}/init.standard-output 2> ${RRRR}/init.standard-error
+    elif ${RESOURCE}/init.sh $( ${CAT} ${RESOURCE}/init.arguments ) > ${RESOURCE}/init.standard-output 2> ${RESOURCE}/init.standard-error
     then
       STATUS=${?}
     else
       STATUS=${?}
     fi &&
-    ${ECHO} ${STATUS} > ${RRRR}/init.status &&
-    ${CHMOD} 0400 ${RRRR}/init.standard-output ${RRRR}/init.standard-error ${RRRR}/init.status
+    ${ECHO} ${STATUS} > ${RESOURCE}/init.status &&
+    ${CHMOD} 0400 ${RESOURCE}/init.standard-output ${RESOURCE}/init.standard-error ${RESOURCE}/init.status
   fi &&
-  if [ ! -z "$( ${CAT} > ${RRRR}/init.standard.error )" ]
+  if [ ! -z "$( ${CAT} > ${RESOURCE}/init.standard.error )" ]
   then
-      ${RRRR}/teardown-asynch.sh &&
+      ${RESOURCE}/teardown-asynch.sh &&
         exit ${ERROR}
   elif [ -z "${STATUS}" ] || [ ${STATUS} == 0 ]
   then
-    ${ECHO} ${TARGET_PID// /} > ${RRRR}/${TARGET_PID// /}.pid &&
-      ${CHMOD} 0400 ${RRRR}/${TARGET_PID// /}.pid
-      ${RRRR}/teardown-asynch.sh &&
-      ${ECHO} ${RRRR}/target
+    ${ECHO} ${TARGET_PID// /} > ${RESOURCE}/${TARGET_PID// /}.pid &&
+      ${CHMOD} 0400 ${RESOURCE}/${TARGET_PID// /}.pid
+      ${RESOURCE}/teardown-asynch.sh &&
+      ${ECHO} ${RESOURCE}/target
   else
-    ${RRRR}/teardown-asynch.sh &&
+    ${RESOURCE}/teardown-asynch.sh &&
       exit ${ERROR}
   fi
