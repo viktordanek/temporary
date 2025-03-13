472ac8f6e4b416d045fdd649c3608e36bfe84854
Victor Danek <viktordanek10@gmail.com>
2025-03-13 18:26:49 -0400

    got rid of debug

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,19 +5,19 @@ source ${MAKE_WRAPPER}/nix-support/setup-hook &&
   ${CHMOD} 0400 ${RESOURCE}/init.arguments &&
   if ${IS_INTERACTIVE}
   then
-    TARGET_PID=${PARENT_PID} && ${ECHO} 1 >> /post/debug
+    TARGET_PID=${PARENT_PID}
   elif ${IS_PIPE}
   then
-    TARGET_PID=${GRANDPARENT_PID} && ${ECHO} 2 >> /post/debug &&
+    TARGET_PID=${GRANDPARENT_PID} &&
       ${TEE} > ${RESOURCE}/init.standard-input &&
       ${CHMOD} 0400 ${RESOURCE}/init.standard-input
   elif ${IS_FILE}
   then
-    TARGET_PID=${GRANDPARENT_PID} && ${ECHO} 3 >> /post/debug &&
+    TARGET_PID=${GRANDPARENT_PID} &&
       ${CAT} > ${RESOURCE}/init.standard-input &&
       ${CHMOD} 0400 ${RESOURCE}/init.standard-input
   else
-    TARGET_PID=${GRANDPARENT_PID} && ${ECHO} 4 >> /post/debug # ${PARENT_PID}
+    TARGET_PID=${GRANDPARENT_PID}
   fi &&
   ${ECHO} ${TARGET_PID// /} > ${RESOURCE}/pid &&
   ${CHMOD} 0400 ${RESOURCE}/pid &&
