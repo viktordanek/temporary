9215322aeb66345332a6c0f6d592873aff9b49c9
Victor Danek <viktordanek10@gmail.com>
2025-03-13 17:47:15 -0400

    finally

--- scripts/vacuum.sh
+++ scripts/vacuum.sh
@@ -1,10 +1,11 @@
-exec 201> /post.lock &&
+${RM} --force ${RESOURCE}/init.sh ${RESOURCE}/setup.sh ${RESOURCE}/teardown-asynch.sh ${RESOURCE}/teardown-synch.sh &&
+  exec 201> /post.lock &&
   if ${FLOCK} 201
   then
     if [ -f /post/index ] && [ -d /post/resource ]
     then
       INDEX=$(( $( ${CAT} /post/index ) + 1 )) &&
-        if [ ! -z "$( ${DIFF} /post/resource ${RESOURCE} )" ]
+        if [ ! -z "$( ${DIFF} --recursive /post/resource ${RESOURCE} )" ]
         then
           ${MV} ${RESOURCE} /post/resource.${INDEX}
         fi &&
