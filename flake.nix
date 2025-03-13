9c0c96b7b1ed9d693756f50c1787f1da1f686330
Victor Danek <viktordanek10@gmail.com>
2025-03-13 10:56:30 -0400

    it correctly handled arguments

--- flake.nix
+++ flake.nix
@@ -600,9 +600,10 @@
                                                             (
                                                                 ignore :
                                                                     {
+                                                                        arguments = "cc5094dbdb456a268a5ba30672881129510d4239be61dfdb553f2f14754bc71094cb9f600b0b7f192e63d7a1b7a61034c554f947dd339cc410ee99eacebe2ccc" ;
                                                                         count = 1 ;
                                                                         expected = self + "/mounts/B0hwDMGO" ;
-                                                                        paste = "022f5919fa3e2909c7057e0511ce754c93d7cd159d84ccbf391ee21b87055e07a6ce8804ffa4def7f5dd1e41145a115f9d8d4ca1704e43236c5e56a8bc22bec3" ;
+                                                                        # paste = "022f5919fa3e2909c7057e0511ce754c93d7cd159d84ccbf391ee21b87055e07a6ce8804ffa4def7f5dd1e41145a115f9d8d4ca1704e43236c5e56a8bc22bec3" ;
                                                                         status = true ;
                                                                     }
                                                             )
