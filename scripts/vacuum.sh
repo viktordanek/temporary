00e8b0dd5f10424f789f37e52c0c78f4ee94c583
Victor Danek <viktordanek10@gmail.com>
2025-03-13 11:39:46 -0400

    WTF

--- scripts/vacuum.sh
+++ scripts/vacuum.sh
@@ -1,27 +1,19 @@
-${ECHO} DAA >> /post/debug &&
 exec 201> /post.lock &&
   if ${FLOCK} 201
   then
-${ECHO} DAB >> /post/debug &&
     if [ -f /counter/index ] && [ -d /cou]
     then
-${ECHO} DAC >> /post/debug &&
-      INDEX=$( ${CAT} /counter/index ) &&
-${ECHO} DAD >> /post/debug &&
+      INDEX=$(( $( ${CAT} /counter/index ) + 1 )) &&
         if [ ! -z "$( ${DIFF} /post/resource ${RESOURCE} )" ]
         then
-${ECHO} DAD >> /post/debug &&
           ${MV} ${RESOURCE} /post/resource.${INDEX}
         fi
     else
       ${ECHO} 1 > /post/index &&
-${ECHO} DAE >> /post/debug &&
         ${MV} ${RESOURCE} /post/resource
     fi &&
-${ECHO} DAF >> /post/debug &&
     ${RM} /post.lock
   else
     ${ECHO} Locking Problem >> /post/lock &&
-${ECHO} DAG >> /post/debug &&
       exit 64
-  fi && ${ECHO} DAH >> /post/debug
+  fi
