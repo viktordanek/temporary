cf0e1db589627880b41daa1a9edb6f2957c9af8b
Victor Danek <viktordanek10@gmail.com>
2025-03-14 14:20:34 -0400

    baby step forward

--- scripts/vacuum.sh
+++ scripts/vacuum.sh
@@ -1,5 +1,6 @@
 ${RM} --force ${RESOURCE}/init.sh ${RESOURCE}/post.sh ${RESOURCE}/release.sh ${RESOURCE}/setup.sh ${RESOURCE}/teardown-asynch.sh ${RESOURCE}/teardown-synch.sh &&
   exec 201> /post.lock &&
+  # ${ECHO} ${0} >> /post/debug &&
   if ${FLOCK} 201
   then
     if [ -f /post/index ]
@@ -11,7 +12,7 @@ ${RM} --force ${RESOURCE}/init.sh ${RESOURCE}/post.sh ${RESOURCE}/release.sh ${R
     ${ECHO} ${INDEX} > /post/index &&
     ${FIND} ${RESOURCE} | while read FILE
     do
-      CAT_NAME=$( ${ECHO} CAT ${INDEX} ${FILE#${RESOURCE}} | ${SHA512SUM} )
+      CAT_NAME=$( ${ECHO} CAT ${INDEX} ${FILE#${RESOURCE}} | ${SHA512SUM}  )
     done &&
     ${MV} ${RESOURCE} /post/resource.${INDEX} &&
     ${RM} /post.lock
