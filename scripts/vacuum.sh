577146835200b1b20131061caadd7fc8e9562e91
Viktor Danek <viktordanek10@gmail.com>
2025-03-14 21:48:04 -0400

    removed resource.1

--- scripts/vacuum.sh
+++ scripts/vacuum.sh
@@ -13,16 +13,20 @@ ${RM} --force ${RESOURCE}/init.sh ${RESOURCE}/post.sh ${RESOURCE}/release.sh ${R
     ${MKDIR} /post/resource.${INDEX} &&
     ${FIND} ${RESOURCE} | while read FILE
     do
-      CAT_NAME=$( ${ECHO} CAT ${INDEX} ${FILE#${RESOURCE}} | ${SHA512SUM} | ${CUT} --bytes -128 ) &&
+      CAT_NAME=$( ${ECHO} CAT 0 ${FILE#${RESOURCE}} | ${SHA512SUM} | ${CUT} --bytes -128 ) &&
         if [ -f ${FILE} ]
         then
           ${CAT} ${FILE} > /post/resource.${INDEX}/${CAT_NAME} &&
             ${CHMOD} 0444 /post/resource.${INDEX}/${CAT_NAME}
         fi &&
-        STAT_NAME=$( ${ECHO} STAT ${INDEX} ${FILE#${RESOURCE}} | ${SHA512SUM} | ${CUT} --bytes -128 ) &&
+        STAT_NAME=$( ${ECHO} STAT 0 ${FILE#${RESOURCE}} | ${SHA512SUM} | ${CUT} --bytes -128 ) &&
         ${STAT} --format="%a" ${FILE} > /post/resource.${INDEX}/${STAT_NAME} &&
           ${CHMOD} 0444 /post/resource.${INDEX}/${STAT_NAME}
     done &&
+    if [ ${INDEX} -gt 0 ] && [ -z "$( ${DIFF} --recursive /post/resource.0 /post/resource.${INDEX} )" ]
+    then
+      ${RM} --recursive --force /post/resource.${INDEX}
+    fi &&
     ${RM} /post/lock
   else
     ${ECHO} Locking Problem >> /post/lock.error &&
