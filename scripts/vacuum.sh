ceddfa9a25bd96b85db2ac154466d89407128eb7
Viktor Danek <viktordanek10@gmail.com>
2025-03-16 19:00:49 -0400

    better

--- scripts/vacuum.sh
+++ scripts/vacuum.sh
@@ -8,40 +8,26 @@ exec 202> /post/.lock &&
         else
           INCREMENT=0
         fi &&
-${ECHO} AAAF ${INCREMENT} ${0} ${RESOURCE} ${STORE} >> /post/yyy &&
         ${ECHO} ${INCREMENT} > /util/increment &&
-${ECHO} AAAG >> /post/yyy &&
         ${MKDIR} /post/resource.${INCREMENT} &&
-${ECHO} AAAH >> /post/yyy &&
         ${FIND} ${RESOURCE} | while read FILE
         do
-${ECHO} AAAI >> /post/yyy &&
           NAME=${FILE#${RESOURCE}} &&
-${ECHO} AAAJ >> /post/yyy &&
             CAT_NAME=$( ${ECHO} CAT 0 ${NAME} | ${SHA512SUM} | ${CUT} --bytes -128 ) &&
-${ECHO} AAAK >> /post/yyy &&
             ${ECHO} ${NAME} > /post/resource.${INCREMENT}/${CAT_NAME}.name &&
-${ECHO} AAAL >> /post/yyy &&
             ${CHMOD} 0444 /post/resource.${INCREMENT}/${CAT_NAME}.name &&
-${ECHO} AAAM >> /post/yyy &&
             if [ -f ${FILE} ]
             then
-${ECHO} AAAN >> /post/yyy &&
               ${CAT} ${FILE} > /post/resource.${INCREMENT}/${CAT_NAME}.cat &&
-${ECHO} AAAO >> /post/yyy &&
                 ${CHMOD} 0444 /post/resource.${INCREMENT}/${CAT_NAME}.cat
             fi &&
-${ECHO} AAAP >> /post/yyy &&
             ${STAT} --format="%a" ${FILE} > /post/resource.${INCREMENT}/${CAT_NAME}.stat &&
-${ECHO} AAAQ >> /post/yyy &&
               ${CHMOD} 0444 /post/resource.${INCREMENT}/${CAT_NAME}.stat
         done &&
         if [ ${INCREMENT} -gt 0 ] && [ -z "$( ${DIFF} --recursive /post/resource.0 /post/resource.${INCREMENT} )" ]
         then
-${ECHO} AAAR >> /post/yyy &&
           ${RM} --recursive --force /post/resource.${INCREMENT}
         fi
   else
-${ECHO} AAAS >> /post/yyy &&
     ${ECHO} >> /post/lock
   fi
