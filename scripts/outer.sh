e2afd6884a161502733a15e5d0a6cbcf3fcc5e91
Viktor Danek <viktordanek10@gmail.com>
2025-03-16 19:15:21 -0400

    good

--- scripts/outer.sh
+++ scripts/outer.sh
@@ -2,5 +2,9 @@ ${INNER} &&
   ${SLEEP} ${TIMEOUT} &&
   exec 203> /post/.lock &&
   ${FLOCK} 203 &&
-  ${FIND} /temporary -mindepth 1 -maxdepth 1 | ${WC} --lines > /post/leaks &&
+  LEAKS=$( ${FIND} /temporary -mindepth 1 -maxdepth 1 | ${WC} --lines ) &&
+  if ${VERBOSE} || [ ${LEAKS} != 0 ]
+  then
+    ${ECHO} ${LEAKS} > /post/leaks
+  fi &&
   ${RM} /post/.lock
