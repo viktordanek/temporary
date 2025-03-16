941ac1ec959c4004b56b0c4c11e7af43fa9a2b94
Viktor Danek <viktordanek10@gmail.com>
2025-03-16 19:07:48 -0400

    yes

--- scripts/outer.sh
+++ scripts/outer.sh
@@ -1,2 +1,6 @@
 ${INNER} &&
-  ${SLEEP} ${TIMEOUT}
+  ${SLEEP} ${TIMEOUT} &&
+  exec 203> /post/.lock &&
+  ${FLOCK} 203 &&
+  ${FIND} /temporary -mindepth 1 -maxdepth 1 | ${WC} --lines > /post/leaks &&
+  ${RM} /post/.lock
