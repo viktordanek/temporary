9bda9248d653f396ed081744d81407cb6b76de84
Viktor Danek <viktordanek10@gmail.com>
2025-03-27 14:17:05 +0000

    restart

--- flake.nix
+++ flake.nix
@@ -1,30 +1,75 @@
 {
     inputs =
         {
-            environment-variable.url = "github:viktordanek/environment-variable" ;
             flake-utils.url = "github:numtide/flake-utils" ;
             nixpkgs.url = "github:NixOs/nixpkgs" ;
             shell-script.url = "github:viktordanek/shell-script" ;
+            string.url = "github:viktordanek/string" ;
             visitor.url = "github:viktordanek/visitor" ;
         } ;
     outputs =
-        { environment-variable , flake-utils , nixpkgs , self , shell-script , visitor } :
+        { flake-utils , nixpkgs , self , shell-script , string , visitor } :
             let
                 fun =
                     system :
                         let
-                            _environment-variable = builtins.getAttr system environment-variable.lib ;
                             lib =
                                 {
                                     host-path ? "\${TMP_DIR}" ,
                                     init ? null ,
+                                    lock-failure ? 64 ,
                                     post ? null ,
                                     release ? null ,
                                     shell-scripts ? { } ,
                                     tests ? null
                                 } :
                                     let
-                                        in null ;
+                                        primary =
+                                            {
+                                                lock-failure =
+                                                    if builtins.typeOf lock-failure == "int" then builtins.toString lock-failure
+                                                    else builtins.throw "lock-failure is not int but ${ builtins.typeOf lock-failure }." ;
+                                            } ;
+                                        scripts =
+                                            let
+                                                _shell-script = builtins.getAttr system shell-script.lib ;
+                                                in
+                                                    {
+                                                        teardown =
+                                                            {
+                                                                setup =
+                                                                    {
+                                                                        true =
+                                                                            {
+                                                                                true =
+                                                                                    _shell-script
+                                                                                        {
+                                                                                            environment =
+                                                                                                { string } :
+                                                                                                    [
+                                                                                                        ( string "ECHO" "${ pkgs.coreutils }/bin/echo" )
+                                                                                                        ( string "FLOCK" "${ pkgs.coreutils }/bin/flock" )
+                                                                                                        ( string "LOCK_FAILURE" primary.lock-failure )
+                                                                                                        ( string "RM" "${ pkgs.coreutils }/bin/rm" )
+                                                                                                        ( string "TAIL" "${ pkgs.coreutils }/bin/tail" )
+                                                                                                        ( string "TRUE" "${ pkgs.coreutils }/bin/true" )
+                                                                                                    ] ;
+                                                                                            extension =
+                                                                                                {
+                                                                                                    string = builtins.getAttr system string.lib ;
+                                                                                                } ;
+                                                                                            name = "teardown" ;
+                                                                                            script = builtins.toFile "teardown" ( builtins.concatStringsSep "" ( builtins.genList ( index : builtins.elemAt ( pkgs.split ( builtins.readFile ( self + "/teardown.sh" ) ) ) index ) [ 1 2 3 4 6 7 8 9 10 11 14 16 17 18 19 20] ) ) ;
+                                                                                        } ;
+                                                                            } ;
+                                                                    } ;
+                                                            } ;
+                                                    } ;
+                                        in
+                                            {
+                                                scripts = scripts ;
+                                            } ;
+                            pkgs = builtins.import nixpkgs { system = system ; } ;
                             in
                                 {
                                     checks =
---
+++ teardown.sh
@@ -0,0 +1,20 @@
+exec 201 > ${RESOURCE}/lock &&
+  if ${FLOCK} 201
+  then
+    ${TAIL} --follow --pid ${PID} &&
+#
+      if ${RELEASE} > ${RESOURCE}/release.standard-output >2 ${RESOURCE}/release.standard-error
+      then
+        ${ECHO} ${?} > ${RESOURCE}/release.status
+      else
+        ${ECHO} ${?} > ${RESOURCE}/release.status
+      fi &&
+#
+#
+      ( ${POST} || ${TRUE} ) &&
+#
+      ${RM} --recursive --force ${RESOURCE}
+  else
+    ${ECHO} FAILED TO LOCK ${RESOURCE}/lock >&2 &&
+      exit ${LOCK_FAILURE}
+  fi
