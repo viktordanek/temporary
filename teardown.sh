6b2ba0f24a72add5a149c025db648cfd81b92d6c
Viktor Danek <viktordanek10@gmail.com>
2025-03-28 11:37:11 +0000

    WTF

--- teardown.sh
+++ teardown.sh
@@ -2,18 +2,17 @@ exec 201> ${RESOURCE}/lock && ${ECHO} ${RESOURCE} >&2 && cat ${0} >&2 &&
   if ${FLOCK} 201
   then
     ${TAIL} --follow --pid ${PID} &&
-#
+# 5
       if ${RELEASE} > ${RESOURCE}/release.standard-output >2 ${RESOURCE}/release.standard-error
       then
         ${ECHO} ${?} > ${RESOURCE}/release.status
       else
         ${ECHO} ${?} > ${RESOURCE}/release.status
       fi &&
-#
-      ${TRUE}
-#
+# 12
+# 13
       ( ${POST} || ${TRUE} ) &&
-#
+# 15
       ${RM} --recursive --force ${RESOURCE}
   else
     ${ECHO} FAILED TO LOCK ${RESOURCE}/lock >&2 &&
