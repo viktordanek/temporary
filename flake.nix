11cd0e0db174ab7b1c88919746635a1c1962e4e2
Viktor Danek <viktordanek10@gmail.com>
2025-04-01 11:09:00 +0000

    retread

--- flake.nix
+++ flake.nix
@@ -245,7 +245,7 @@
                                                             ( foobar "post" post true )
                                                             ( foobar "release" release true )
                                                             ( foobar "teardown-0-0-0" ( builtins.getAttr "teardown" ( builtins.getAttr "scripts" ( lib { } ) ) ) true )
-                                                            # ( foobar "teardown-0-0-1" ( builtins.getAttr "teardown" ( builtins.getAttr "scripts" ( lib { post = post.shell-script ; } ) ) ) false )
+                                                            ( foobar "teardown-0-0-1" ( builtins.getAttr "teardown" ( builtins.getAttr "scripts" ( lib { post = post.shell-script ; } ) ) ) true )
                                                             # ( foobar "teardown-0-1-0" ( builtins.getAttr "teardown" ( builtins.getAttr "scripts" ( lib { release = release.shell-script ; } ) ) ) true )
                                                             # ( foobar "teardown-0-1-1" ( builtins.getAttr "teardown" ( builtins.getAttr "scripts" ( lib { release = release ; post = post ; } ) ) ) true )
                                                             # ( foobar "teardown-1-0-0" ( builtins.getAttr "teardown" ( builtins.getAttr "scripts" ( lib { init = init ; } ) ) ) true )
