6858cce5aa7e5d3c5ef073b7fbdfff92537de87f
Viktor Danek <viktordanek10@gmail.com>
2025-04-01 20:38:13 +0000

    wtf

--- flake.nix
+++ flake.nix
@@ -306,7 +306,7 @@
                                                             ( foobar "teardown-0-1-0" ( builtins.getAttr "teardown" ( builtins.getAttr "scripts" ( lib { release = release.shell-script ; } ) ) ) true )
                                                             ( foobar "teardown-0-1-1" ( builtins.getAttr "teardown" ( builtins.getAttr "scripts" ( lib { release = release.shell-script ; post = post.shell-script ; } ) ) ) true )
                                                             ( foobar "teardown-1-0-0" ( builtins.getAttr "teardown" ( builtins.getAttr "scripts" ( lib { init = init.shell-script ; } ) ) ) true )
-                                                            ( foobar "teardown-1-0-0" ( builtins.getAttr "setup" ( builtins.getAttr "scripts" ( lib { init = init.shell-script ; } ) ) ) false )
+                                                            # ( foobar "teardown-1-0-0" ( builtins.getAttr "setup" ( builtins.getAttr "scripts" ( lib { init = init.shell-script ; } ) ) ) false )
                                                             ( foobar "teardown-1-0-1" ( builtins.getAttr "teardown" ( builtins.getAttr "scripts" ( lib { post = post.shell-script ; } ) ) ) true )
                                                             ( foobar "teardown-1-1-0" ( builtins.getAttr "teardown" ( builtins.getAttr "scripts" ( lib { release = release.shell-script ; } ) ) ) true )
                                                             ( foobar "teardown-1-1-1" ( builtins.getAttr "teardown" ( builtins.getAttr "scripts" ( lib { init = init.shell-script ; release = release.shell-script ; post = post.shell-script ; } ) ) ) true )
