411e88b252737f1b388b02041d37250d0786ce10
Viktor Danek <viktordanek10@gmail.com>
2025-04-03 11:46:14 +0000

    WTF

--- flag.sh
+++ flag.sh
@@ -1,4 +1,9 @@
-${TOUCH} /target &&
+if ${TOUCH} /target > /dev/null 2>&1
+then
+  ${ECHO} did touch /target
+else
+  ${ECHO} did not touch /target
+fi &&
   ${FIND} /resource /target -type f | ${SORT} | while read FILE
   do
     ${ECHO} ${FILE} &&
@@ -7,5 +12,10 @@ ${TOUCH} /target &&
   done &&
   if [ ${NAME} == "init" ]
   then
-    ${ECHO} ${UUID} > /mount/target
+    if ${ECHO} ${UUID} > /mount/target
+    then
+      ${ECHO} did touch /mount/target
+    else
+      ${ECHO} did not touch /mount/target
+    fi
   fi
