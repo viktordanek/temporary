3525d83d87456947ebc942af0ece7ed030b64791
Viktor Danek <viktordanek10@gmail.com>
2025-04-07 18:36:02 +0000

    baby progress

--- test/external.sh
+++ test/external.sh
@@ -11,7 +11,7 @@ export CANDIDATE=${1} &&
   ${TOUCH} /build/archive/FLAG &&
   ( ${INTERNAL} ${CANDIDATE} & ) &&
   ${FIND} /build &&
-  echo ${CAT} /build/initial.d23b6bd5ae72a50ccf19180b2c8b72f616ef51da34a2111daaf2cc80e99a3492c0eebd3b3745f46b5d77442f562b55251b4131906f05991b8d46c5cde6df08d6/target &&
+  ${STAT} /build/initial.d23b6bd5ae72a50ccf19180b2c8b72f616ef51da34a2111daaf2cc80e99a3492c0eebd3b3745f46b5d77442f562b55251b4131906f05991b8d46c5cde6df08d6/target &&
   ${FIND} /build/resources &&
   ${SLEEP} 10s &&
   ${FIND} /build/archive
