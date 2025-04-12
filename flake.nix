b088ad2222bfbd686658414b7628f3d2aa284a3a
Viktor Danek <viktordanek10@gmail.com>
2025-04-12 17:20:17 -0400

    YES

--- flake.nix
+++ flake.nix
@@ -579,8 +579,7 @@
                                                                                         else
                                                                                             ${ pkgs.coreutils }/bin/echo There was error in ${ temporary.tests }. >&2 &&
                                                                                                 exit 62
-                                                                                        fi &&
-                                                                                        exit 61
+                                                                                        fi
                                                                                 '' ;
                                                                             name = name ;
                                                                             src = ./. ;
@@ -607,9 +606,9 @@
                                                             } ;
                                                     in
                                                         [
-                                                            # ( foobar "0-0" ( lib { init = init ; tests = tests ; } ) )
-                                                            # ( foobar "0-1" ( lib { init = init ; post = post ; tests = tests ; } ) )
-                                                            # ( foobar "1-0" ( lib { init = init ; release = release ; tests = tests ; } ) )
+                                                            ( foobar "0-0" ( lib { init = init ; tests = tests ; } ) )
+                                                            ( foobar "0-1" ( lib { init = init ; post = post ; tests = tests ; } ) )
+                                                            ( foobar "1-0" ( lib { init = init ; release = release ; tests = tests ; } ) )
                                                             ( foobar "1-1" ( lib { init = init ; release = release ; post = post ; tests = tests ; } ) )
                                                         ] ;
                                             post =
