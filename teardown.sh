c8c40d5e9517925375d9b84d51ce17e98718d856
Viktor Danek <viktordanek10@gmail.com>
2025-04-12 14:26:15 -0400

    all but setup ... the most important one

--- teardown.sh
+++ teardown.sh
@@ -1,20 +1,20 @@
 exec 201> /mount/${RESOURCE_NAME}/lock &&
   if ${FLOCK} 201
   then
-   ${ECHO} teardown B >> /archive/DEBUG ${ECHO} teardown b >> /mount/debug && if [ ${STATUS} == 0 ]
+   if [ ${STATUS} == 0 ]
     then
-      ${TAIL} --follow /dev/null --pid ${ORIGINATOR_PID} && ${ECHO} teardown C >> /archive/DEBUG
+      ${TAIL} --follow /dev/null --pid ${ORIGINATOR_PID}
     fi && export RESOURCE=/mount/${RESOURCE_NAME} &&
 #
     if ${RELEASE} > /mount/${RESOURCE_NAME}/release.standard-output 2> /mount/${RESOURCE_NAME}/release.standard-error
     then
-      ${ECHO} ${?} > /mount/${RESOURCE_NAME}/release.status && ${ECHO} teardown D >> /archive/DEBUG
+      ${ECHO} ${?} > /mount/${RESOURCE_NAME}/release.status
     else
-      ${ECHO} ${?} > /mount/${RESOURCE_NAME}/release.status && ${ECHO} teardown E >> /archive/DEBUG
+      ${ECHO} ${?} > /mount/${RESOURCE_NAME}/release.status
     fi &&
 #
 #
-       ${ECHO} teardown M >> /archive/DEBUG && ( ${POST} || ${TRUE} ) && ${ECHO} teardown >&2 && ${ECHO} teardown N >> /archive/DEBUG &&
+       ( ${POST} || ${TRUE} ) &&
 #
       ${RM} --recursive --force /mount/${RESOURCE_NAME}
   else
--- vacuum.sh
+++ vacuum.sh
@@ -1,26 +1,15 @@
-${ECHO} vacuum A >> /archive/DEBUG &&
 ${FIND} /resource | ${SORT} | while read FILE
   do
-${ECHO} vacuum B >> /archive/DEBUG &&
     KEY=${FILE#/resource} &&
-${ECHO} vacuum C >> /archive/DEBUG &&
       HASH=$( ${ECHO} ${KEY} | ${SHA512SUM} | ${CUT} --bytes -128 ) &&
-${ECHO} vacuum C >> /archive/DEBUG &&
       INDEX=$( ${FIND} /archive -mindepth 2 -maxdepth 2 -type f -name ${HASH}.key | ${WC} --lines ) &&
-${ECHO} vacuum D >> /archive/DEBUG &&
       ${MKDIR} --parents /archive/${INDEX} &&
-${ECHO} vacuum E >> /archive/DEBUG &&
       ${ECHO} ${KEY} > /archive/${INDEX}/${HASH}.key &&
-${ECHO} vacuum F >> /archive/DEBUG &&
       ${STAT} --format "%a" ${FILE} > /archive/${INDEX}/${HASH}.stat &&
-${ECHO} vacuum G >> /archive/DEBUG &&
       ${CHMOD} 0777 /archive/${INDEX}/${HASH}.key /archive/${INDEX}/${HASH}.stat &&
-${ECHO} vacuum H >> /archive/DEBUG &&
       if [ -f ${FILE} ]
       then
-${ECHO} vacuum I>> /archive/DEBUG &&
         ${CAT} ${FILE} > /archive/${INDEX}/${HASH}.stat &&
-${ECHO} vacuum J >> /archive/DEBUG &&
           ${CHMOD} 0777 /archive/${INDEX}/${HASH}.stat
       fi
   done
