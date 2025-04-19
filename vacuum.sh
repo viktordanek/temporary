c1562116b981a210a59f75e78339f216ac6361eb
Viktor Danek <viktordanek10@gmail.com>
2025-04-19 09:43:58 -0400

    YES

--- vacuum.sh
+++ vacuum.sh
@@ -4,6 +4,5 @@ INDEX=$( ${FIND} /archive -mindepth 1 -maxdepth 1 -type d | ${WC} --lines ) &&
     ${ECHO} We assumed that /archive/${INDEX} did not exist >&2 &&
       exit 64
   fi &&
-  # ${CP} --recursive /resource /archive/${INDEX} &&
-  # ${SED} -i "s#^export ORIGINATOR_PID='.*'\$#export ORIGINATOR_PID='\${ORIGINATOR_PID}'#" /archive/${INDEX}/cat
-  true
+  ${CP} --recursive /resource /archive/${INDEX} &&
+  ${SED} -i "s#^export ORIGINATOR_PID='.*'\$#export ORIGINATOR_PID='\${ORIGINATOR_PID}'#" /archive/${INDEX}/teardown.sh
