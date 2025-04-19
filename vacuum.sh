e355b49295ac9f6282241c2bb490494e07a279e0
Viktor Danek <viktordanek10@gmail.com>
2025-04-19 07:27:18 -0400

    this seems OK except for teardown.sh :  RESOURCE_NAME and RESOURCES

--- vacuum.sh
+++ vacuum.sh
@@ -1,16 +1,7 @@
-${ECHO} AA >> /archive/FLAG &&
-  INDEX=$( ${FIND} /archive -mindepth 1 -maxdepth 1 -type d | ${WC} --lines ) &&
-${ECHO} AB >> /archive/FLAG &&
+INDEX=$( ${FIND} /archive -mindepth 1 -maxdepth 1 -type d | ${WC} --lines ) &&
   if [ -e /archive/${INDEX} ]
   then
-${ECHO} AC >> /archive/FLAG &&
     ${ECHO} We assumed that /archive/${INDEX} did not exist >&2 &&
-${ECHO} AD >> /archive/FLAG &&
       exit 64
   fi &&
-${ECHO} AE >> /archive/FLAG &&
-${ECHO} ${CP} --recursive /resource /archive/${INDEX} >> /archive/FLAG &&
-ls -lah / >> /archive/FLAG &&
-${ECHO} BA >> /archive/FLAG &&
-ls -lah /resource >> /archive/FLAG &&
   ${CP} --recursive /resource /archive/${INDEX}
