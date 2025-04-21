cc70aa5ab6a0ebd80670304e0b3ed7d3fb50a6bd
Viktor Danek <viktordanek10@gmail.com>
2025-04-21 07:06:41 -0400

    it seemed to work with sleep parameter

--- flake.nix
+++ flake.nix
@@ -545,7 +545,8 @@
                                                                 with-index = builtins.genList ( index : { index = index ; line = builtins.elemAt all index ; } ) ( builtins.length all ) ;
                                                                 filtered = builtins.filter ( x : builtins.any ( i : x.index == i ) array ) with-index ;
                                                                 simplified = builtins.map ( x : x.line ) filtered ;
-                                                                in builtins.toFile "setup" ( builtins.concatStringsSep "\n" simplified ) ;git
+                                                                in builtins.toFile "setup" ( builtins.concatStringsSep "\n" simplified ) ;
+                                                        sleep = 10 ;
                                                         tests = primary.tests ;
                                                     } ;
                                         setup = setup-fun false primary.self-teardown teardown ;
