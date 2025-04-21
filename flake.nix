9c77a4c8362add1b0f0a2829c38b438acc5cfc42
Viktor Danek <viktordanek10@gmail.com>
2025-04-21 08:40:52 -0400

    this is harder than i expected because we are using status in setup

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
                                                         failure =
                                                             ignore :
                                                                 {
-                                                                    archive = self + "/expected/setup/failure/mounts/archive/${ builtins.toJSON release }" ;
+                                                                    archive = self + "/expected/setup/failure/mounts/archive/${ builtins.toJSON release }/${ builtins.toJSON init }" ;
                                                                     arguments = "fd4107d952c0d02f4ea2e8963d673543791619d2ff0178d03222ea551c539c235a516d9f6dbb2c852618c634ead3ebc72d6beff6ee08880d422e10341390a94c" ;
                                                                     status = 66 ;
                                                                 } ;
