017ba90364857001b8646658b2e74e1bdc987334
Viktor Danek <viktordanek10@gmail.com>
2025-05-01 07:28:09 -0400

    WTF

--- teardown.sh
+++ teardown.sh
@@ -1,20 +1,23 @@
-exec 201 > ${RESOURCE}/lock &&
+exec 201> /mount/${RESOURCE_NAME}/lock &&
   if ${FLOCK} 201
   then
-    ${TAIL} --follow --pid ${PID} &&
+   if [ ${STATUS} == 0 ]
+    then
+      ${TAIL} --follow /dev/null --pid ${ORIGINATOR_PID}
+    fi && export RESOURCE=/mount/${RESOURCE_NAME} &&
 #
-      if ${RELEASE} > ${RESOURCE}/release.standard-output >2 ${RESOURCE}/release.standard-error
-      then
-        ${ECHO} ${?} > ${RESOURCE}/release.status
-      else
-        ${ECHO} ${?} > ${RESOURCE}/release.status
-      fi &&
+    if ${RELEASE} > /mount/${RESOURCE_NAME}/release.standard-output 2> /mount/${RESOURCE_NAME}/release.standard-error
+    then
+      ${ECHO} ${?} > /mount/${RESOURCE_NAME}/release.status
+    else
+      ${ECHO} ${?} > /mount/${RESOURCE_NAME}/release.status
+    fi &&
 #
 #
-      ( ${POST} || ${TRUE} ) &&
+       ( ${POST} || ${TRUE} ) &&
 #
-      ${RM} --recursive --force ${RESOURCE}
+      ${RM} --recursive --force /mount/${RESOURCE_NAME}
   else
-    ${ECHO} FAILED TO LOCK ${RESOURCE}/lock >&2 &&
+    ${ECHO} FAILED TO LOCK /mount/${RESOURCE_NAME}/lock >&2 &&
       exit ${LOCK_FAILURE}
   fi
